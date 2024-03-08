d49944ecb9a7adf204a9f2975140ab00a6674790
Jeremyddsmith <jeremyddsmith@gmail.com>
2024-03-08 11:55:20 -0500

    Second Commit

---
+++ weeks_to_live.py
@@ -0,0 +1,8 @@
+age = "**" #Change me to utilize the calculator
+# Your code below this line 👇
+years = 90 - int(age)
+weeks = years * 52
+
+print(f"You have {weeks} weeks left.")
+
+#This simple exercise showcases the utilization of "f" strings and how much they can cut down on extra manual conversions.
