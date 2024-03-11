fe47e071e30621647bc49113078fe08c5eba4c1c
Jeremyddsmith <jeremyddsmith@gmail.com>
2024-03-11 16:59:08 -0400

    Updated for more clarity

--- tip-calculator.py
+++ tip-calculator.py
@@ -2,13 +2,11 @@ print("Hello, welcome to your automatic tip calculator!")
 # This line will provide you with your bill amount
 bill = float(input("How much was your bill?\n"))
 # here we are finding out how many ways we will be splitting the bill
-guests = input("How many guests did you dine with today?\n")
-guestint = int(guests)
+guests = int(input("How many guests did you dine with today?\n"))
 ## We are choosing out tip amount here
-percentage = input("What percent tip would you like to leave? 10 15 20 or custom\n")
-percentint = int(percentage)
+percentage = int(input("What percent tip would you like to leave? 10 15 20 or custom\n"))
 # Here we are going to present our final bill total and our individual contribution amount
-totalbill = float((percentint / 100 * bill) + bill)
+totalbill = float((percentage / 100 * bill) + bill)
 print(f"Your total bill is {totalbill: .2f}")
-individual = (totalbill / guestint)
+individual = (totalbill / guests)
 print(f"Your total individual contribution is {individual: .2f}")
